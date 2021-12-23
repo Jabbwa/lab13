3a37876c532d14a2ff77ffb384b7d88678c0fdb8
Jabbwa <sopida_kit@cmu.ac.th>
2021-12-24 01:21:01 +0700

    complete 1

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,30 @@
 #include<iostream>
+#include <ctime> 
 using namespace std;
+
+int  fibonacci(int);
 
 int main(){
     int x;
+    cout << "Input num : ";
     cin >> x;
     cout << fibonacci(x);
     return 0; 
 }
+
+int fibonacci(int x)
+{
+    if (x == 1 || x == 0 ) return x;
+    else if (x>1)
+    {
+        return fibonacci(x-1)+fibonacci(x-2);
+    }
+    else return 0;
+}
+
+/*F_0 = 0
+F_1 = 1
+F_2 = F_1+F_0 =1
+F_3 = F_2+F_1 = 1+1 =2
+F_4 = F_3+F_2 = 2+1 =3 
+... */
